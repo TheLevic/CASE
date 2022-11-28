bd1639c5c6629000159bb9e553d3d1acbc094d02
TheLevic <levicrider08@gmail.com>
2022-11-28 11:44:27 -0600

    Testing

--- frontend/case-web/src/App.js
+++ frontend/case-web/src/App.js
@@ -1,14 +1,30 @@
-import "./App.css";
-import { Alert, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Alert variant="success">This is a button</Alert>
-        <Button>Test Button</Button>
-      </header>
+    <div className="container">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
