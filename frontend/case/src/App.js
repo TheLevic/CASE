9ee0e1cf6635df1a525f0c5bb5a4b98ae377cc0f
TheLevic <levicrider08@gmail.com>
2023-01-06 12:10:03 -0600

    New UI Design

---
+++ frontend/case/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Heading from "./components/Heading";
+import Videos from "./components/Videos";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Videos />
+    </div>
+  );
+}
+
+export default App;
