f22f1dcc20ad6790047003a33a408c09cfb824e0
TheLevic <lccrider@uark.edu>
2023-01-16 15:02:28 -0600

    Cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
