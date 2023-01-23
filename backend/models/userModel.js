3f62d0dbf19699c14b46c8fa364cd83c4607faea
TheLevic <levicrider08@gmail.com>
2023-01-23 10:21:22 -0600

    Adding admin feature

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,6 +14,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    admin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
   { timestamps: true }
 );
