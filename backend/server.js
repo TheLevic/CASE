652ec3a6258d1ea7348b0182990214a5c9558caa
TheLevic <levicrider08@gmail.com>
2023-01-23 11:06:16 -0600

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const express = require("express");
 const PORT = process.env.PORT || 8000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 // Connect to DB
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
