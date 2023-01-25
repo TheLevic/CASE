0917dfa40053abbc850ff81c0465298ccece100f
TheLevic <levicrider08@gmail.com>
2023-01-25 15:14:29 -0600

    Updated Heading

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,29 +5,29 @@ function Header() {
       <header className=" bg-gray-100 grid grid-cols-2">
         <a
           href="/"
-          className="text-green-400 self-middle text-3xl p-1 font-bold "
+          className="text-green-400 self-middle text-3xl md:text-6xl p-1 font-bold "
         >
           CASE
         </a>
-        <div className="flex flex-row justify-end items-center m-1 gap-4">
+        <div className="flex flex-row justify-end items-center m-1 gap-4 lg:gap-6">
           <a
             href="/"
-            className="bg-green-400 rounded-lg p-1 text-white font-semibold"
+            className="bg-green-400 rounded-lg p-1 md:p-2 text-white font-medium lg:text-2xl"
           >
             Videos
           </a>
           <a
             href="/"
-            className="bg-green-400 rounded-lg p-1 text-white font-semibold"
+            className="bg-green-400 rounded-lg p-1 md:p-2 text-white font-medium lg:text-2xl"
           >
             Images
           </a>
           <input
             type="text"
-            className="hidden sm:block w-full p-1 font-semibold rounded-lg"
+            className="hidden sm:block w-full p-1 md:p-2 font-medium lg:text-2xl rounded-lg"
             placeholder="Search"
           />
-          <button className="hidden sm:block bg-green-400 p-1 rounded-lg font-semibold text-white">
+          <button className="hidden sm:block bg-green-400 font-medium md:p-2 lg:text-2xl rounded-lg text-white">
             Search
           </button>
         </div>
