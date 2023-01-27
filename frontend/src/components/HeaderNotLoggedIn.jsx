34c0580f781ba8d52eb2607bc97a122e1fc5ba35
TheLevic <levicrider08@gmail.com>
2023-01-27 13:56:03 -0600

    Added basic HeaderNotLoggedin

--- frontend/src/components/HeaderNotLoggedIn.jsx
+++ frontend/src/components/HeaderNotLoggedIn.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import LinkButton from "./LinkButton";
 
 function HeaderNotLoggedIn() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2">
+      <a
+        href="/"
+        className="text-green-400 self-middle text-3xl md:text-6xl p-1 font-bold "
+      >
+        CASE
+      </a>
+      <div className="flex flex-row justify-end items-center m-1 gap-4 lg:gap-6">
+        <LinkButton link="/" text="Sign Up" />
+        <LinkButton link="/" text="Login" />
+      </div>
+      <div className="border-b-4 pt-1 border-gray-400 col-span-2"></div>
+    </div>
+  );
 }
 
 export default HeaderNotLoggedIn;
