78dcdecfde9cf290d71f73460ccd6dce3ae5786c
Levi Crider <levicrider08@gmail.com>
2023-02-16 14:23:24 -0600

    Small Changes. Added listeners

--- CASE/Case.py
+++ CASE/Case.py
@@ -5,7 +5,7 @@ from picamera2 import Picamera2
 
 class Camera:
     def __init__(self):
-        self.gpio_button_pin = 25
+        self.gpio_button_pin = 2
         self.camera = Picamera2()
         self.button = Button(self.gpio_button_pin)
         self.video_config = self.camera.create_video_configuration(main={"size": (1920,1080)})
@@ -15,31 +15,32 @@ class Camera:
         self.name = str(self.num) + ".h264"
         self.shouldRecord = False
 
+    # Method that will start recording
     def start_recording(self):
         if self.shouldRecord:
             self.camera.start_recording(self.encoder,self.name)
             time.sleep(1)
             print("Starting to record")
 
+    # Method that will stop recording
     def stop_recording(self):
         self.camera.stop_recording()
         print("Recording Stopped")
         self.num += 1
         self.shouldRecord = False
 
-    def callBack():
-        print("Hello!")
-        time.sleep(2)
-
+    # Listener that waits for button to call start_recording
     def start_recording_on_press(self):
-        self.button.when_pressed = self.callBack()
+        self.button.wait_for_press()
+        self.start_recording()
 
+    # Listener that waits for button press to call stop_recording
     def stop_recording_on_press(self):
-        self.button.when_pressed = self.callBack() 
+        self.button.wait_for_press()
+        self.stop_recording()
 
 if __name__ == "__main__":
     camera = Camera()
     while (True):
        camera.start_recording_on_press()  
-       time.sleep(2)
        camera.stop_recording_on_press();
